66c763db1f544f3e53970772f19274c8f186b0bf
Bigdanfo <ktpss9717827@gmail.com>
2024-05-16 21:21:38 +0800

    game

--- N09310028 GUI_Class/N09310028 GUI_Class/FormGame.Designer.cs
+++ N09310028 GUI_Class/N09310028 GUI_Class/FormGame.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -47,35 +48,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.Location = new System.Drawing.Point(263, 131);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 12);
+            this.label1.Size = new System.Drawing.Size(402, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = "已隨機產生0~100數字，請在";
+            this.label1.Text = "已隨機產生0~100數字，請在下方作答";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label2.Location = new System.Drawing.Point(263, 167);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.Size = new System.Drawing.Size(210, 24);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "請輸入0~100正整數";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(306, 225);
+            this.button2.Location = new System.Drawing.Point(306, 214);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 49);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "猜";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(495, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
             // 
             // FormGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -93,5 +106,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
